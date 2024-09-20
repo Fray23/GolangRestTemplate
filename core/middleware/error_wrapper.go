693fa4c39cd0879e161a9e704af1d6fdc6a67b7b
Marat <mars6358@gmail.com>
2024-09-20 23:02:01 +0600

    add error log

--- core/middleware/error_wrapper.go
+++ core/middleware/error_wrapper.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,10 +13,12 @@ func ErrorWrapper [T any](handlerFunc func(c *gin.Context) (T, error)) gin.Handl
 	return func(c *gin.Context) {
 		response_dto, err := handlerFunc(c)
 		if err != nil {
+
 			switch err {
 			case auth_err.UserAlreadyRegistered:
 				c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 			default:
+			    fmt.Println(err)
 				c.JSON(http.StatusInternalServerError, gin.H{"error": "unexpected error occurred"})
 			}
 			return
