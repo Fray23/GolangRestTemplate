9c0a9144bfc6d6194a971604a856b7f40b30e766
Marat <mars6358@gmail.com>
2024-09-20 23:00:58 +0600

    add bycrypt hasher

---
+++ core/security/hash.go
@@ -0,0 +1,21 @@
+package security
+
+import (
+    "golang.org/x/crypto/bcrypt"
+)
+
+const (
+	MinCost     int = 4
+	MaxCost     int = 31
+	DefaultCost int = 10
+)
+
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), DefaultCost)
+    return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
